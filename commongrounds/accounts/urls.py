ed3c6cb0e068a6ddf4eacaf9b512370c08f8f5c9
felizemendoza <felize.maria.mendoza@student.ateneo.edu>
2026-05-08 23:14:55 +0800

    debug

--- commongrounds/accounts/urls.py
+++ commongrounds/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ProfileUpdateView, ProfileRegisterView
 
 urlpatterns = [
-    path('<str:username>/', ProfileUpdateView.as_view(), name="profile_update"),
     path('register/', ProfileRegisterView.as_view(), name="profile_register"),
+    path('<str:username>/', ProfileUpdateView.as_view(), name="profile_update"),
 ]
 app_name = 'accounts'
--- commongrounds/commongrounds/settings.py
+++ commongrounds/commongrounds/settings.py
@@ -19,7 +19,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-CSRF_TRUSTED_ORIGINS = ["https://commongrounds-f-20.up.railway.app/"]
+CSRF_TRUSTED_ORIGINS = ["https://commongrounds-f-20.up.railway.app/",]
 
 
 # Quick-start development settings - unsuitable for production
@@ -32,9 +32,6 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = [
-    "https://commongrounds-f-20-production.up.railway.app/",
-]
 
 # "commongrounds-f-20-production.up.railway.app"
 
