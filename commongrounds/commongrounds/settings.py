5c2e146eb791e6f6098329f256bc93c139bd8572
Elite-TS <venicci.mikhail.palmiano@student.ateneo.edu>
2026-05-08 17:18:25 +0800

    Tweaked Settings for Deployment
    Edited settings for deployment

--- commongrounds/commongrounds/settings.py
+++ commongrounds/commongrounds/settings.py
@@ -18,6 +18,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+CSRF_TRUSTED_ORIGINS = ["https://commongrounds-f-20.up.railway.app/"]
 
 
 # Quick-start development settings - unsuitable for production
@@ -29,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["'commongrounds-f-20.up.railway.app'"]
 
 
 # Application definition
