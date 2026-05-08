1934606e31f41d2bfe1179de2a2e0bd6ca2dd5b5
Elite-TS <venicci.mikhail.palmiano@student.ateneo.edu>
2026-05-08 16:58:46 +0800

    Started Implementing CSS Styling
    Implemented CSS files from Phoebe.

--- commongrounds/commongrounds/urls.py
+++ commongrounds/commongrounds/urls.py
@@ -18,9 +18,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from .views import HomePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomePageView.as_view(), name='homepage'),
     path('localevents/', include('localevents.urls', namespace='localevents')),
     path('bookclub/', include('bookclub.urls', namespace='bookclub')),
     path('merchstore/', include('merchstore.urls', namespace="merchstore")),
