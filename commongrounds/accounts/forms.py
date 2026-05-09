c379bf0809871aa25d7132259af1e2a5be64c579
felizemendoza <felize.maria.mendoza@student.ateneo.edu>
2026-05-09 13:58:15 +0800

    debug

--- commongrounds/accounts/forms.py
+++ commongrounds/accounts/forms.py
@@ -18,6 +18,7 @@ class UserForm(UserCreationForm):
 
 class ProfileForm(forms.ModelForm):
     class Meta:
+        model = Profile
         role = forms.ChoiceField(
             choices=[
                 ("None", "None"),
