1e458b385c994197a4514b8c3b160b8006645b7b
felizemendoza <felize.maria.mendoza@student.ateneo.edu>
2026-05-09 13:37:15 +0800

    debug

--- commongrounds/accounts/forms.py
+++ commongrounds/accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Profile
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm 
 
 
 class ProfileUpdateForm(forms.ModelForm):
@@ -9,10 +10,10 @@ class ProfileUpdateForm(forms.ModelForm):
         fields = ['display_name']
 
 
-class ProfileRegisterForm(forms.ModelForm):
+class ProfileRegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password1', 'password2']
 
     role = forms.ChoiceField(
         choices=[
